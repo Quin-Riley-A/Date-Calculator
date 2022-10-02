2f61803dcfac7b1fdb53ecffa2f31c2275e39077
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-02 14:47:13 -0400

    update test for complete line coverage
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- __tests__/date.tests.js
+++ __tests__/date.tests.js
@@ -11,9 +11,12 @@ describe('Date', () => {
   test('should convert date to Zeller Algorithm appropriate format', () => {
     const date = new Date('2022-10-01');
     const date2 = new Date('2022-01-01');
+    const date3 = new Date('2022-02-01');
     date.dateZeller();
     date2.dateZeller();
+    date3.dateZeller();
     expect(date.zellerDate).toEqual('2022-10-01');
     expect(date2.zellerDate).toEqual('2021-13-01');
+    expect(date3.zellerDate).toEqual('2021-14-01');
   });
 });
