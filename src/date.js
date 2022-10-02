4e17ef900578cb9925dde8c5c50e2bd15cd3781e
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-02 15:47:19 -0400

    pass initial condition for test 3 of Date object
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- src/date.js
+++ src/date.js
@@ -18,6 +18,13 @@ export default class Date{
   };
   
   zellerCalc(){
-
+    const zellerDateArr = this.zellerDate.split('-');
+    "2000-11-12"
+    ["2000", "11", "12"]
+    const zellerYear = parseInt((zellerDateArr[0]).slice(-2));
+    const zellerDay = parseInt(zellerDateArr[2]);
+    const zellerMonth = parseInt(zellerDateArr[1]);
+    const zellerCentury = Math.floor(parseInt(zellerDateArr[0])/100);
+    this.dayValue = (Math.floor(13*(zellerMonth + 1)/5) + Math.floor(zellerYear/4) + Math.floor(zellerCentury/4) + zellerDay + zellerYear - 2*zellerCentury) % 7;
   }; 
 }
