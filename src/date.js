9fe27dc40a7f5f0916c7ed9c2a144a61c8d4e8e3
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-02 14:35:44 -0400

    pass test2 of date TDD
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- src/date.js
+++ src/date.js
@@ -4,9 +4,16 @@ export default class Date{
     this.year = dateArr[0];
     this.month = dateArr[1];
     this.day = dateArr[2];
-  }
+  };
 
-  insertMethodName(){
-
-  }
+  dateZeller(){
+    let intYear = parseInt(this.year);
+    if (this.month === "01") {
+      this.zellerDate = (intYear - 1) + "-" + "13" + "-" + this.day;
+    } else if (this.month === "02") {
+      this.zellerDate = (intYear - 1) + "-" + "14" + "-" + this.day;
+    }else {
+      this.zellerDate = this.year + "-" + this.month + "-" + this.day;
+    };
+  };
 }
