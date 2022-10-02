9588630df0dad532bb443037ba290421317a1d4e
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-02 14:34:30 -0400

    establish good fail for dateZeller method
    Co-authored-by: Athea DeLing <almdeling@gmail.com> Co-authored-by: Greg Ramsower <greg.ramso
    
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>
    Co-authored-by: Najma Abdi <anajmabdi@gmail.com>
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>

--- __tests__/date.tests.js
+++ __tests__/date.tests.js
@@ -7,4 +7,13 @@ describe('Date', () => {
     expect(date.month).toEqual("10");
     expect(date.day).toEqual("01");
   });
+
+  test('should convert date to Zeller Algorithm appropriate format', () => {
+    const date = new Date('2022-10-01');
+    const date2 = new Date('2022-01-01');
+    date.dateZeller();
+    date2.dateZeller();
+    expect(date.zellerDate).toEqual('2022-10-01');
+    expect(date2.zellerDate).toEqual('2021-13-01');
+  });
 });
